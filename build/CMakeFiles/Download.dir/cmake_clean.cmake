736f69d18937fd9e4ef19f28e83bce859f842d71
zichua <376944569@qq.com>
2021-03-29 19:07:00 +0800

    1

---
+++ build/CMakeFiles/Download.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Download.dir/HttpDown.cpp.o"
+  "CMakeFiles/Download.dir/HttpDown.cpp.o.d"
+  "CMakeFiles/Download.dir/HttpOperation.cpp.o"
+  "CMakeFiles/Download.dir/HttpOperation.cpp.o.d"
+  "CMakeFiles/Download.dir/ThreadPool.cpp.o"
+  "CMakeFiles/Download.dir/ThreadPool.cpp.o.d"
+  "CMakeFiles/Download.dir/main.cpp.o"
+  "CMakeFiles/Download.dir/main.cpp.o.d"
+  "Download"
+  "Download.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Download.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Download.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Download.
+# This may be replaced when dependencies are built.
